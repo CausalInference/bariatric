61f4dae2d46155f7b1fceeb829c2e83fbf3130bc
Arin Madenci <37673730+arinmadenci@users.noreply.github.com>
2024-09-26 13:52:07 -0400

    Update main-bootstrap-single-interval-analyses.R

--- main-bootstrap-single-interval-analyses.R
+++ main-bootstrap-single-interval-analyses.R
@@ -4,7 +4,7 @@ source(here("clean-full-data_functions.R"))
 source(here("analysis-functions", "model_formulas.R"))
 source(here("analysis-functions", "single-interval-functions", "bootstrap-single-interval-main-function.R"))
 
-single_interval_results <- bootstrap_single_interval_function(seed_vector=1:2, number.processors=1,
+single_interval_results <- bootstrap_single_interval_function(seed_vector=0:1000, number.processors=1,
                                    followup_years=7, censor_surgery=TRUE,
                                    stabilized=TRUE, trim=TRUE,
                                    ipw.formula_n = NULL, 
