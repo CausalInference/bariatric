06cfd4c4298f9acbb6b190047cc68341f31abae3
Arin Madenci <37673730+arinmadenci@users.noreply.github.com>
2024-09-26 13:51:51 -0400

    Update main-bootstrap-grace-period-analyses.R

--- main-bootstrap-grace-period-analyses.R
+++ main-bootstrap-grace-period-analyses.R
@@ -5,7 +5,7 @@ source(here("analysis-functions", "model_formulas.R"))
 source(here("analysis-functions", "gp-functions", "bootstrap_gp_wrapper.R"))
 
 
-gp_results <- bootstrap_gp_wrapper_function(bootstrap=TRUE, seed_vector=1:2, name_dat="diabetics-cv", number.processors=1,
+gp_results <- bootstrap_gp_wrapper_function(bootstrap=TRUE, seed_vector=0:1000, name_dat="diabetics-cv", number.processors=1,
                                             grace.intervals=12, min.interval=6, trim=TRUE, 
                                             censor_surgery=TRUE, followup_years=7,
                                             ipw.formula_d = paste0("y_surgery ~ ",  formulas$final_model_diabetics)
